93420a4b557f85246e1b8c617128aa588b605bbd
Ed Southey <me@edsouthey.com>
2012-04-04 11:07:13 +0100

    ES: changing to use UTF-8 loading provided by framework

--- app/plugin/PropertiesFileConfigurationPlugin.java
+++ app/plugin/PropertiesFileConfigurationPlugin.java
@@ -7,23 +7,17 @@ import java.util.Properties;
 import play.Logger;
 import play.Play;
 import play.PlayPlugin;
+import play.libs.IO;
 
 public class PropertiesFileConfigurationPlugin extends PlayPlugin {
 
 	public void onApplicationStart() {
 		String propertiesFilename = Play.configuration.getProperty("externalConfig.fileName", "/" + Play.id + ".properties");
-
 		Logger.info("Loading configuration from " + propertiesFilename);
-
-		try {
-			Properties properties = new Properties();
-			properties.load(this.getClass().getResourceAsStream(propertiesFilename));
-
-			for (Entry<Object, Object> entry : properties.entrySet()) {
-				Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Unable to read .properties from classpath");
+		Properties properties = IO.readUtf8Properties(this.getClass().getResourceAsStream(propertiesFilename));
+		
+		for (Entry<Object, Object> entry : properties.entrySet()) {
+			Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
 		}
 	}
 }
