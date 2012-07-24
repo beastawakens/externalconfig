2d2e987233c94ee99825ac102ffd219397a7b39a
Neoh59 <neoh59@free.fr>
2012-07-24 17:25:56 +0200

    add absolute path feature and catch error for readUtf8Properties method

--- app/plugin/PropertiesFileConfigurationPlugin.java
+++ app/plugin/PropertiesFileConfigurationPlugin.java
@@ -1,5 +1,8 @@
 package plugin;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -14,13 +17,33 @@ public class PropertiesFileConfigurationPlugin extends PlayPlugin {
 	@Override
 	public void onConfigurationRead() {
 		String[] propertiesFilenames = Play.configuration.getProperty("externalConfig.fileName", "/" + Play.id + ".properties").split(",");
+		String propertiesFileAbsolutePath = Play.configuration.getProperty("externalConfig.fileAbsolutePath");
 		
 		for (String propertiesFilename : propertiesFilenames) {
-			Logger.info("Loading configuration from " + propertiesFilename);
-			Properties properties = IO.readUtf8Properties(this.getClass().getResourceAsStream(propertiesFilename));
+			String propertiesFilenameAndPath = null;
+			InputStream is = null;
+			if (propertiesFileAbsolutePath != null) {
+				propertiesFilenameAndPath = propertiesFileAbsolutePath + System.getProperty("file.separator") + propertiesFilename;
+				try {
+					is = new FileInputStream(propertiesFilenameAndPath);
+				} catch (FileNotFoundException fnfe) {
+					Logger.error("Not found configuration file " + propertiesFilenameAndPath);
+				}
+			} else {
+				// get from classpath
+				propertiesFilenameAndPath = propertiesFilename;
+        is = this.getClass().getResourceAsStream(propertiesFilenameAndPath);
+      }
+			Logger.info("Loading configuration from " + propertiesFilenameAndPath);
 			
-			for (Entry<Object, Object> entry : properties.entrySet()) {
-				Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
+			try  {
+				Properties properties = IO.readUtf8Properties(is);
+				
+				for (Entry<Object, Object> entry : properties.entrySet()) {
+					Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
+				}
+			} catch (Exception e) {
+				Logger.error("Error when loading file " + propertiesFilenameAndPath);
 			}
 		}
 	}
