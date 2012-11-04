4112452c4ac3c867ce0866c5d7856ff94a9d2b62
Paul (Hoogheemraad) <hoogheemraad@outlook.com>
2012-11-04 21:53:32 +0100

    Merging 2 commits from Neoh79. Ran checkstyle over them and fixed a hardcoded module path in the sample.

--- app/plugin/PropertiesFileConfigurationPlugin.java
+++ app/plugin/PropertiesFileConfigurationPlugin.java
@@ -3,7 +3,6 @@ package plugin;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Properties;
 
@@ -12,40 +11,51 @@ import play.Play;
 import play.PlayPlugin;
 import play.libs.IO;
 
+/**
+ * This class takes care of reading additional properties from external sources.
+ * @author Rugbyhead (Original)
+ * @author Hoogheemraad
+ * @author Neoh79
+ */
 public class PropertiesFileConfigurationPlugin extends PlayPlugin {
-	
-	@Override
-	public void onConfigurationRead() {
-		String[] propertiesFilenames = Play.configuration.getProperty("externalConfig.fileName", "/" + Play.id + ".properties").split(",");
-		String propertiesFileAbsolutePath = Play.configuration.getProperty("externalConfig.fileAbsolutePath");
-		
-		for (String propertiesFilename : propertiesFilenames) {
-			String propertiesFilenameAndPath = null;
-			InputStream is = null;
-			if (propertiesFileAbsolutePath != null) {
-				propertiesFilenameAndPath = propertiesFileAbsolutePath + System.getProperty("file.separator") + propertiesFilename;
-				try {
-					is = new FileInputStream(propertiesFilenameAndPath);
-				} catch (FileNotFoundException fnfe) {
-					Logger.error("Not found configuration file " + propertiesFilenameAndPath);
-				}
-			} else {
-				// get from classpath
-				propertiesFilenameAndPath = propertiesFilename;
-        is = this.getClass().getResourceAsStream(propertiesFilenameAndPath);
-      }
-			Logger.info("Loading configuration from " + propertiesFilenameAndPath);
-			
-			try  {
-				Properties properties = IO.readUtf8Properties(is);
-				
-				for (Entry<Object, Object> entry : properties.entrySet()) {
-					Play.configuration.setProperty((String) entry.getKey(),(String) entry.getValue());
-				}
-			} catch (Exception e) {
-				Logger.error("Error when loading file " + propertiesFilenameAndPath);
-			}
-		}
-	}
+
+    /**
+     * This method is automatically called when application.conf has been parsed
+     * and loaded.
+     */
+    public final void onConfigurationRead() {
+        String[] propertiesFilenames = Play.configuration.getProperty("externalConfig.fileName",
+                "/" + Play.id + ".properties").split(",");
+        String propertiesFileAbsolutePath = Play.configuration.getProperty("externalConfig.fileAbsolutePath");
+
+        for (String propertiesFilename : propertiesFilenames) {
+            String propertiesFilenameAndPath = null;
+            InputStream is = null;
+            if (propertiesFileAbsolutePath != null) {
+                propertiesFilenameAndPath = propertiesFileAbsolutePath + System.getProperty("file.separator")
+                        + propertiesFilename;
+                try {
+                    is = new FileInputStream(propertiesFilenameAndPath);
+                } catch (FileNotFoundException fnfe) {
+                    Logger.error("Not found configuration file " + propertiesFilenameAndPath);
+                }
+            } else {
+                // get from classpath
+                propertiesFilenameAndPath = propertiesFilename;
+                is = this.getClass().getResourceAsStream(propertiesFilenameAndPath);
+            }
+            Logger.info("Loading configuration from " + propertiesFilenameAndPath);
+
+            try {
+                Properties properties = IO.readUtf8Properties(is);
+
+                for (Entry<Object, Object> entry : properties.entrySet()) {
+                    Play.configuration.setProperty((String) entry.getKey(), (String) entry.getValue());
+                }
+            } catch (Exception e) {
+                Logger.error("Error when loading file " + propertiesFilenameAndPath);
+            }
+        }
+    }
 
 }
